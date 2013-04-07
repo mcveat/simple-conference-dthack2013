152616fb552d9370a9f3898fe20e7586cd6a700d
Piotr Adamski <mcveat@gmail.com>
2013-04-07 03:23:21 +0200

    evernote agenda made awesome with markdown

--- app/client/Evernote.scala
+++ app/client/Evernote.scala
@@ -9,6 +9,7 @@ import com.evernote.thrift.transport.THttpClient
 import com.evernote.thrift.protocol.TBinaryProtocol
 import com.evernote.edam.`type`.Note
 import com.evernote.edam.notestore.NoteStore.Client
+import org.markdown4j.Markdown4jProcessor
 
 /**
  * User: mcveat
@@ -20,6 +21,7 @@ object Evernote {
   val config = Config.Evernote
   val service = new ServiceBuilder().provider(classOf[Sandbox]).apiKey(config.key).apiSecret(config.secret)
     .callback(config.callback).build()
+  val markdown = new Markdown4jProcessor
   def getRequestToken = {
     val t = service.getRequestToken
     (t.getToken, t.getSecret)
@@ -44,22 +46,19 @@ object Evernote {
     val note = new Note
     note.setTitle("Conference call at %s" format c.date)
     val contactsList = c.contacts.map { contact =>
-      val number = "<b>%s</b> " format contact.number
-      val email = contact.email.map("<i>%s</i>" format _).getOrElse("")
-      "<li>%s%s</li>".format(number, email)
+      "<li><b>%s</b> <i>%s %s</i></li>".format(contact.name, contact.number, contact.email.getOrElse(""))
     }.mkString
     note.setContent(
       """<?xml version="1.0" encoding="UTF-8"?>
         | <!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">
         | <en-note>
-        |   <h1>You're invited to conference call "%s"</h1>
-        |   Conference is scheduled at %s.
+        |   <h1>%s</h1>
         |   <h2>Agenda</h2>
         |   <div>%s</div>
         |   <h2>Participants</h2>
         |   <div>%s</div>
         | </en-note>
-      """.stripMargin.format(c.title, c.date, c.agenda, contactsList))
+      """.stripMargin.format(c.title, markdown.process(c.agenda), contactsList))
     note
   }
 }
